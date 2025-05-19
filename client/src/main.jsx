e4dcd78949a04e5d77d86dbb5e1e623a085232e3
haseeb2412 <haseeb241202@gmail.com>
2025-05-20 02:20:01 +0500

    solving deployment error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { TransactionProvider } from "./context/Transactioncontext.jsx";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from 'react-router-dom';
+
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <TransactionProvider>
       <StrictMode>
         <App />
       </StrictMode>
     </TransactionProvider>
-  </BrowserRouter>
+    </HashRouter>
 );
